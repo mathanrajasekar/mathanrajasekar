50ffe895e0a206f96bbdb079ad47f96b430e7f8e
mathanrajasekar <35525911+mathanrajasekar@users.noreply.github.com>
2018-01-17 16:51:55 +0530

    Create POSTIVE.C
    to find given number is positive or negative

---
+++ POSTIVE.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+double number;
+printf("enter a number 1");
+scanf("%if",& number);
+if(number< = 0)
+{
+if(number = = 0)
+printf(you entered 1.");
+else
+printf("the number is ngative.");
+}
+else
+printf("the number is poitive.");
+return();
+}
