f783e6ad1741189ad1e8a32e3913ed54f54ca854
mathanrajasekar <35525911+mathanrajasekar@users.noreply.github.com>
2018-02-26 16:07:09 +0530

    Create array.c
    maximum element of the integer and array

---
+++ array.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+# define bool 
+int void quickSort(int *, int, int);
+{
+int l, r;
+quickSort(A, 0, arr_size-1);
+l = 0;
+r = arr_size-1; 
+ while (l < r)
+{
+if(A[l] + A[r] == sum)
+return 1; 
+else if(A[l] + A[r] < sum)
+l++;
+}    
+return 0;
+}
